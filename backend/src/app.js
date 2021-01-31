2af877c75e00175c912c8d6c42c78871e2866d6e
hgoldenberg <hgoldenberg@hotmail.com>
2021-01-31 18:36:12 -0300

    setting, routes and middleware basic

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// settngs
+app.set("port", process.env.PORT || 4000);
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.post("/api/register", (req, res) => res.send("register route"));
+app.post("/api/login", (req, res) => res.send("login route"));
+app.get("/api/users", (req, res) => res.send("users route"));
 
 module.exports = app;
